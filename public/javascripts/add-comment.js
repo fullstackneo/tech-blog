97e5b6f798f2d9df35b10a837837cd6a877c9b00
Neo <fullstackneo@gmail.com>
2022-05-17 14:44:06 -0600

    fix bug in add-comment.js, not rendering comment

--- public/javascripts/add-comment.js
+++ public/javascripts/add-comment.js
@@ -1,24 +1,10 @@
-const req = require("express/lib/request");
-
-const textareaEl = document.querySelector('textarea');
-// let isExceed = false;
-let commentStr = textareaEl.value;
-// textareaEl.addEventListener('keyup', () => {
-//   commentStr = textareaEl.value;
-//   console.log(300 - commentStr.length);
-//   console.log(isExceed);
-//   if (commentStr.length > 300) {
-//     isExceed = true;
-//   }
-//   document.querySelector('')
-// });
-
 async function createPostHandler(event) {
   event.preventDefault();
   const post_id = location.pathname.split('/')[2];
-  const comment_text = commentStr;
+  const comment_text = document.querySelector('textarea').value;
+  
   if (post_id && comment_text) {
-    fetch('/api/comments', {
+    await fetch('/api/comments', {
       method: 'post',
       body: JSON.stringify({
         post_id,
